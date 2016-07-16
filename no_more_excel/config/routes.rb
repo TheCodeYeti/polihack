2bc3be350babc768fd3006408e108255b79b09ac
spencer <spencer.dossett@gmail.com>
2016-07-16 14:48:59 -0400

    added users controller

--- no_more_excel/config/routes.rb
+++ no_more_excel/config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/show'
+
+  get 'users/update'
+
+  get 'users/edit'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
